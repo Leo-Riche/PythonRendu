91bfff2e38d7a7a7927230ec930a892c2d166296
Leo-Riche <leo.riche@edu.devinci.fr>
2024-10-22 16:52:23 +0200

    Updating Phase 1

--- scrapping.py
+++ scrapping.py
@@ -4,7 +4,11 @@ import csv
 
 url = "https://books.toscrape.com/"
 response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response.content, 'html.parser')
+
+category_url = url + soup.select("ul > li > a")[33]["href"]
+response = requests.get(category_url)
+soup = BeautifulSoup(response.content, 'html.parser')
 
 book = soup.select("article > div > a")
 
@@ -15,7 +19,6 @@ soup = BeautifulSoup(response.text, 'html.parser')
 book_infos = soup.select("td")
 book_url = url
 book_upc = book_infos[0].text
-book_product_type = book_infos[1].text
 book_title = soup.select("h1")[0].text
 book_price_including_taxe = book_infos[3].text.replace('Â', '')
 book_price_excluding_taxe = book_infos[2].text.replace('Â', '')
@@ -27,5 +30,5 @@ book_img_url = "books.toscrape.com" + soup.select("img")[0]["src"].replace('../.
 
 with open('csv/book_data.csv', mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
-    writer.writerow(['URL', 'UPC', 'Product Type', 'Title', 'Price (incl. tax)', 'Price (excl. tax)', 'Availability', 'Description', 'Category', 'Review Rating', 'Image URL'])
-    writer.writerow([book_url, book_upc, book_product_type, book_title, book_price_including_taxe, book_price_excluding_taxe, book_number_available, book_description, book_category, book_review_rating, book_img_url])
+    writer.writerow(['URL', 'UPC', 'Title', 'Price (incl. tax)', 'Price (excl. tax)', 'Availability', 'Description', 'Category', 'Review Rating', 'Image URL'])
+    writer.writerow([book_url, book_upc, book_title, book_price_including_taxe, book_price_excluding_taxe, book_number_available, book_description, book_category, book_review_rating, book_img_url])
